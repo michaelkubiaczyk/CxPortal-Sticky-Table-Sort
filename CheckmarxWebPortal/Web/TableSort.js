0871d2d805963221dd49404869136e4c348538fa
Michael Kubiaczyk <Michael.Kubiaczyk@checkmarx.com>
2021-06-15 12:43:05 +0200

    Added fix to tableshort.js, support for 9.3

--- CheckmarxWebPortal/Web/TableSort.js
+++ CheckmarxWebPortal/Web/TableSort.js
@@ -1,11 +1,3 @@
-/*
-
-    Author: Michael Kubiaczyk @Checkmarx
-    Target version: Checkmarx 9.2
-    Github: https://github.com/michaelkubiaczyk/CxPortal-Sticky-Table-Sort
-
-*/
-
 var storedParamName = "fs_params";
 
 var tableParams = ["filter", "sort", "order", "pageSize", "showPage" ];
@@ -16,6 +8,7 @@ var targetFilters = new Map();
 var targetState = new URLSearchParams("");
 var targetReached = 1;
 var MTV;
+// Telerik.Web.UI.Grid.Sort($find('ctl00_cpmain_ProjectsGrid_ctl00'), 'LastScanned'); return false;__doPostBack('ctl00$cpmain$ProjectsGrid$ctl00$ctl02$ctl02$ctl28','')
 
 var columnSortRegex = /Telerik\.Web\.UI\.Grid\.Sort\(.+\),\s+'(\w+)'\)\;/ ;
 
@@ -31,19 +24,23 @@ function MTVCreated( grid, args ) {
 	currentState.set( "showPage", MTV.get_currentPageIndex()+1 );
 
 	if ( !currentState.has( "sort" ) || currentState.get( "sort" ) == "" ) {
+		//alert( "Checking sorts?" );
 		var sorts = document.getElementsByClassName( "rgHeader rgSorted" );
 		if ( sorts.length > 0 ) {
 			for ( var i = 0; i < sorts[0].childNodes.length; i++ ) {
 				var n = sorts[0].childNodes[i];
 				if ( n.nodeName == 'INPUT' ) {
+					//alert( n.title );
 					if ( n.title.match(/Sorted desc/i) ) 
 						currentState.set( "order", "DESC" );
 					else 
 						currentState.set( "order", "ASC" );
-
+					
+					//alert( "Checking: " + String(n.onclick) );
 					var col = columnSortRegex.exec(String(n.onclick));
 					if ( col !== null ) {
 						currentState.set( "sort", col[1] );
+						//alert( "Currently sorting: " + col[1] );
 					}
 					break;
 				}
@@ -51,6 +48,8 @@ function MTVCreated( grid, args ) {
 		}
 	}
 	
+//	alert( "Current state: " + currentState.toString() + "\nTarget: " + targetState.toString() );
+
 	var diffs = 0;
 	for ( var i = 0; i < tableParams.length; i++ ) {
 		var p = tableParams[i];
@@ -67,13 +66,19 @@ function MTVCreated( grid, args ) {
 			sessionStorage.removeItem( storedParamName );
 		}
 	}
+	
+
+	
 }
 
 function updateTable( param, value ) {
-//	Need to set param to value
+	//alert( "Need to set param " + param + " to " + value );
 	switch (param) {
 		case "filter":
+			//alert("Updating Filters:\nFrom: " + getFilterString( currentFilters ) + "\nTo: " + getFilterString( targetFilters ) );
 			for ( var [key, filter] of targetFilters ) {
+				//alert( "Want to set filter " + key + " to " + filter + "\nCurrently: " + currentFilters.get( key ) );
+				
 				if ( !currentFilters.has( key ) || currentFilters.get(key) !== filter ) {					
 					var parameters = filter.split( "|?" );
 					setTimeout( function(){ MTV.showFilterItem(); MTV.filter( parameters[0], parameters[1], parameters[2]); }, 500 );
@@ -94,6 +99,7 @@ function updateTable( param, value ) {
 			MTV._sortExpressions.clear();
 
 			if ( sort == "" ) {
+				//alert ( "trying to sort with empty sort: " + sort + ", " + order );
 				sort = currentState.get( "sort" );
 				order = "";
 			}
@@ -111,16 +117,18 @@ function updateTable( param, value ) {
 			setTimeout( function(){ MTV.sort( sort + " " + order ); }, 500 );
 			break;
 		case "page":
-			MTV.set_pageSize( parseInt(value) );
+        case "showPage":
+			if ( value > MTV.get_pageCount() ) value = MTV.get_pageCount();
+			MTV.set_currentPageIndex( value );			
 			break;
 		case "pageSize":
-			if ( value > MTV.get_pageCount() ) value = MTV.get_pageCount();
-			MTV.set_currentPageIndex( value );
-			break;
+			MTV.set_pageSize( parseInt(value) );
+            break;
 	}
 }
 
 function GridCommand( grid, args ) {
+	//alert( "Command: " + args.get_commandName() + ", params: " + args.get_commandArgument() );
 	if ( args.get_commandName() == "Sort" ) {
 		var sort = args.get_commandArgument();
 		var res = sort.split(" ");
@@ -152,9 +160,12 @@ function GridCommand( grid, args ) {
 		currentState.set( "showPage", args.get_commandArgument() );
 	} else if ( args.get_commandName() == "Filter" ) {
 		var filter = args.get_commandArgument().split( "|?", 3 );
-		if ( filter[1] == "" ) { 
+		//alert( "Filtering: " + filter[0] + ", " + filter[1] + ", " + filter[2] );
+		if ( filter[1] == "" ) { // want to unfilter this column
+			//currentState.delete( "filter" );
 			currentFilters.delete( filter[0] );
 		} else {
+			//currentState.set( "filter", args.get_commandArgument() );
 			currentFilters.set( filter[0], args.get_commandArgument() );			
 			currentState.set( "filter", getFilterString( currentFilters ) );
 		}
@@ -199,17 +210,25 @@ function sanitize( str ) {
 function onPageLoad() { 
 	// If the page is loaded with a hash present, save that hash
 	// If the page loads without a hash present, but there's one saved, load the saved one
+//	alert( "OnLoad" );
 	if (window.location.hash == "" && typeof(Storage) !== "undefined" && sessionStorage.getItem( storedParamName ) != null ) {
 		targetHash = sanitize( sessionStorage.getItem( storedParamName ) );
+//		alert( "Loaded hash: " + targetHash + "\nSanitized: " + sanitize(targetHash) );
 		targetState = new URLSearchParams( targetHash );
 		targetReached = 0;
 		sessionStorage.removeItem( storedParamName );
+//		alert( "Loaded stored URL: " + targetHash );
 	}
 	
 	if (window.location.hash != "" ) {
 		var dec = decodeURIComponent(window.location.hash.substring(1));
 		targetHash = sanitize(dec);
+		//alert( "Window hash: " + window.location.hash.substring(1) + "\ndecoded: " + dec + "\nsanitied: " + targetHash	) 
 		targetState = new URLSearchParams( targetHash );
+		
+		
+		//alert( "Target filters from " + targetState.get("filter") + "\n" + getFilterString( targetFilters ) );		
+		
 		sessionStorage.setItem( storedParamName, targetHash );
 		targetReached = 0;
 	}
